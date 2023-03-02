b20f7bceb5920d7cf843b64ae96645051402cd10
huangmi <huangmi@newrank.cn>
2023-03-02 16:20:47 +0800

    练习

--- 01_server.js
+++ 01_server.js
@@ -1,16 +1,65 @@
+// 1. 引入 url 模块
+var url = require("url");
+
+// 2. 引入 http 模块
 var http = require("http");
 
+// 3. 用 http 模块创建服务
+/**
+ * req 获取 url 信息 (request)
+ * res 浏览器返回响应信息 (response)
+ */
 http
-  .createServer(function (request, response) {
-    // 发送 HTTP 头部
-    // HTTP 状态值: 200 : OK
-    // 内容类型: text/plain
-    response.writeHead(200, { "Content-Type": "text/plain" });
+  .createServer(function (req, res) {
+    // 4. 获取服务器请求
+    /**
+     * 访问地址是：http://localhost:3000/?userName=jsliang&userAge=23
+     * 如果你执行 console.log(req.url)，它将执行两次，分别返回下面的信息：
+     * /  ?userName=jsliang&userAge=23
+     * /  /favicon.ico
+     * 这里为了防止重复执行，所以排除 req.url == /favicon.ico 的情况
+     */
+    if (req.url != "/favicon.ico") {
+      // 5. 使用 url 的 parse 方法
+      /**
+       * parse 方法需要两个参数：
+       * 第一个参数是地址
+       * 第二个参数是 true 的话表示把 get 传值转换成对象
+       */
+      var urlObj = url.parse(req.url, true);
+      console.log(urlObj);
+      /**
+       * Url {
+       *   protocol: null,
+       *   slashes: null,
+       *   auth: null,
+       *   host: null,
+       *   port: null,
+       *   hostname: null,
+       *   hash: null,
+       *   search: '?userName=jsliang&userAge=23',
+       *   query: { userName: 'jsliang', userAge: '23' },
+       *   pathname: '/',
+       *   path: '/?userName=jsliang&userAge=23',
+       *   href: '/?userName=jsliang&userAge=23' }
+       */
 
-    // 发送响应数据 "Hello World"
-    response.end("Hello World\n");
+      console.log(urlObj.query.userName); // jsliang
+
+      console.log(urlObj.query.userAge); // 23
+    }
+
+    // 设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf8
+    res.writeHead(200, {
+      "Content-Type": "text/html;charset=UTF-8",
+    });
+
+    // 往页面打印值
+    res.write('<h1 style="text-align:center">Hello NodeJS</h1>');
+
+    // 结束响应
+    res.end();
   })
   .listen(8888);
 
-// 终端打印如下信息
 console.log("Server running at http://127.0.0.1:8888/");
---
+++ 05_stream.js
@@ -0,0 +1,49 @@
+var fs = require("fs");
+var data = "今天天气真好，就是穿厚了有点热";
+
+// ##从流中读取数据
+// 创建可读流
+// var readerStream = fs.createReadStream("input.txt");
+
+// // 设置编码为 utf8
+// readerStream.setEncoding("UTF8");
+
+// // 处理流事件 --> data, end, and error
+// readerStream.on("data", function (chunk) {
+//   data += chunk;
+// });
+
+// readerStream.on("end", function () {
+//   console.log(data);
+// });
+
+// readerStream.on("error", function (err) {
+//   console.log(err.stack);
+// });
+
+// console.log("程序执行完毕");
+
+// ##写入流
+// var writerStream = fs.createWriteStream("output.txt");
+// writerStream.write(data, "utf-8");
+// writerStream.end();
+// writerStream.on("finish", function () {
+//   console.log("写入数据完成");
+// });
+// writerStream.on("error", function (err) {
+//   console.log(err.stack);
+// });
+// console.log("程序执行完毕");
+
+// ##管道流
+// 创建一个可读流
+var readerStream = fs.createReadStream("input.txt");
+
+// 创建一个可写流
+var writerStream = fs.createWriteStream("output.txt");
+
+// 管道读写操作
+// 读取 input.txt 文件内容，并将内容写入到 output.txt 文件中
+readerStream.pipe(writerStream);
+
+console.log("程序执行完毕");
